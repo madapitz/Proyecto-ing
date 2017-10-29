b856bd6f695828ef0b8e8c8a07a10d212c986714
gabrielbaron16 <32284026+gabrielbaron16@users.noreply.github.com>
2017-10-29 12:47:30 -0400

    Update PantallaInicio.php

--- PantallaInicio.php
+++ PantallaInicio.php
@@ -3,52 +3,17 @@
 <html>
 <head>
 	<title>Done!</title>
-<style>
-.boton{
-  -webkit-border-radius: 13;
-  -moz-border-radius: 13;
-  border-radius: 13px;
-  font-family: Arial;
-  color: ##234861;
-  font-size: 41px;
-  background: #5d96ab;
-  padding: 21px 20px 16px 20px;
-  text-decoration: none;
-  margin: 5px;
-  position: relative;
-  left: 38%;
-  top: 150px;
-  display: block;
-}
-.boton:hover{
-  background: #3cb0fd;
-  background-image: -webkit-linear-gradient(top, #3cb0fd, #144b70);
-  background-image: -moz-linear-gradient(top, #3cb0fd, #144b70);
-  background-image: -ms-linear-gradient(top, #3cb0fd, #144b70);
-  background-image: -o-linear-gradient(top, #3cb0fd, #144b70);
-  background-image: linear-gradient(top, bottom, #3cb0fd, #144b70);
-  text-decoration: none;
-}
-#img{
-  width: 20%;
-  height: 10%;
-}
-#logo{
-  position: relative;
-  left: 38%;
-}
-</style>
 <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"/> <!--font-family: 'Quicksand', sans-serif;-->
 <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet"/> <!--font-family: 'Space Mono', monospace;-->
 <link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet"/> <!--font-family: 'Megrim', cursive;-->
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/> <!--font-family: 'Roboto', sans-serif;-->	
+<link rel="stylesheet" href="estilos2.css"/>
 <link rel="stylesheet" href="estilos3.css"/>
 </head>
 <body>
-  <div id="fondo">
-  <div id="logo">
-    <img src="Logo.jpg">
-  </div>
+  <!--<div id="logo">-->
+    <img class="logo" src="Logo.jpg">
+  <!--</div>-->
   <form  action="PantallaInicio.php" method="POST">
    <input name="Registro" type="submit" class="boton" value="Registrar Usuario"  /> <br>
     <input name="Inicio" type="submit" class="boton" value="Iniciar Sesion"/> <br>
